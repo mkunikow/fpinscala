b4fd540cc2c59a97cc70e9c199b461e76fc863ab
mkunikowski <michal.kunikowski@gmail.com>
2015-03-09 06:37:48 +0100

    datastructures

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.collection.mutable.ListBuffer
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 case class Cons[+A](head: A, tail: List[A]) extends List[A] // Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`, which may be `Nil` or another `Cons`.
@@ -53,17 +55,50 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(h, t) => t
     }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] =  l match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_,t) => Cons(h,t)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = {
+    n match {
+      case 0 => l
+      case i => drop(tail(l), n-1)
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h,t) if f(h) => dropWhile(t,f)
+    case Cons(h,t) if !f(h) => l
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init2[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => Nil
+      case Cons(h,t) => Cons(h,init2(t))
+    }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def init[A](l: List[A]): List[A] = {
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+    def initAcc(l: List[A], acc: ListBuffer[A]): List[A] = l match {
+      case Cons(h, Nil) => List(acc.toList: _*)
+      case Cons(h,t) => initAcc(t, acc += h)
+    }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+    l match {
+      case Nil => Nil
+      case _ => initAcc(l, ListBuffer())
+    }
+  }
+
+  def length[A](l: List[A]): Int = foldRight(l, 0)((x,y) => 1 + y)
+
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =  l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z,h))(f)
+  }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
--- exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -9,7 +9,7 @@ import org.scalatest.{FeatureSpec, Matchers}
  */
 
 class ListTest  extends FeatureSpec with Matchers {
-  feature ("The tail"){
+  feature ("The tail function"){
     import fpinscala.datastructures.List._
     scenario ("should return tail of the list"){
 
@@ -26,4 +26,95 @@ class ListTest  extends FeatureSpec with Matchers {
       }
     }
   }
+
+
+  feature ("The head function"){
+    import fpinscala.datastructures.List._
+    scenario ("should return list with new head"){
+
+      val dataset =
+        Table(
+          ("lst",    "head" , "out"       ),
+          (List(1,2), 3,      List(3, 2)  ),
+          (List(2),   1,      List(1)     )
+        )
+
+      forAll (dataset) { (lst: List[Any], head: Any, out: List[Any]) =>
+        setHead(lst, head) shouldEqual out
+      }
+    }
+  }
+
+  feature ("The drop function"){
+    import fpinscala.datastructures.List._
+    scenario ("should return list with new head"){
+
+      val dataset =
+        Table(
+          ("lst",        "n",   "out"      ),
+          (List(1,2,3),   1,    List(2,3)  ),
+          (List(1),       1,    Nil        ),
+          (List(),        1,    Nil        ),
+          (List(),       -1,    Nil        )
+        )
+
+      forAll (dataset) { (lst: List[Any], n: Int, out: List[Any]) =>
+        drop(lst, n) shouldEqual out
+      }
+    }
+  }
+
+  feature ("The dropWhile function"){
+    import fpinscala.datastructures.List._
+    scenario ("should drop all elements while condition is true"){
+
+      val dataset =
+        Table(
+          ("lst",           "f",               "out"        ),
+          (List(-1,-2,3),   (x: Int) => x < 0,  List(3)     ),
+          (List(-1,-2,-3),  (x: Int) => x < 0,  List()      ),
+          (List(1,2,3),     (x: Int) => x < 0,  List(1,2,3) )
+        )
+
+      forAll (dataset) { (lst: List[Int], f: Int => Boolean, out: List[Any]) =>
+        dropWhile(lst, f) shouldEqual out
+      }
+    }
+  }
+
+
+  feature ("The init function"){
+    import fpinscala.datastructures.List._
+    scenario ("should return all elements except last one"){
+
+      val dataset =
+        Table(
+          ("lst",            "out"      ),
+          (List(1,2,3),       List(1,2) )
+        )
+
+      forAll (dataset) { (lst: List[Any], out: List[Any]) =>
+        init(lst) shouldEqual out
+      }
+    }
+  }
+
+
+  feature ("The length function"){
+    scenario ("should return the length of the list"){
+
+      val dataset =
+        Table(
+          ("lst",       "out" ),
+          (List(1,2,3),  3    ),
+          (List(),       0    )
+        )
+
+      forAll (dataset) { (lst: List[Any], out: Int) =>
+        List.length(lst) shouldEqual out
+      }
+    }
+  }
+
+
 }
