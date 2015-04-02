e9235c9edf83f7562e8ee163e445982e275af4e9
mkunikowski <michal.kunikowski@gmail.com>
2015-04-03 01:25:49 +0200

    datastructures tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -13,12 +13,12 @@ object Tree {
 
   def maximum(t: Tree[Int]): Int = t match {
     case Leaf(a) => a
-    case Branch(a,b) => math.max(maximum(a), maximum(b))
+    case Branch(a,b) => maximum(a) max maximum(b)
   }
 
   def depth[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 1
-    case Branch(a, b) => 1 + math.max(depth(a), depth(b))
+    case Branch(a, b) => 1 + (depth(a) max depth(b))
   }
 
   def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
@@ -26,13 +26,16 @@ object Tree {
     case Branch(a,b) => Branch(map(a)(f), map(b)(f))
   }
 
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = ???
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(a,b) => g(fold(a)(f)(g), fold(b)(f)(g))
+  }
 
-  def sizeViaFold[A](t: Tree[A]): Int = ???
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_=>1)((x,y)=>x+y+1)
 
-  def maximumViaFold(t: Tree[Int]): Int = ???
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(x => x)(_ max _)
 
-  def depthViaFold[A](t: Tree[A]): Int = ???
+  def depthViaFold[A](t: Tree[A]): Int = fold(t)(_=>1)((x,y) => 1 + (x max y))
 
-  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(x=>Leaf(f(x)):Tree[B])((x,y) => Branch(x, y))
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -11,55 +11,76 @@ import org.scalatest.{FeatureSpec, Matchers}
 class TreeTest extends FeatureSpec with Matchers {
   feature ("The size function"){
     import fpinscala.datastructures.Tree._
-    scenario ("should return the size of the tree"){
 
-      val dataset =
-        Table(
-          ("tree"                                    ,"size"  ),
-          (Leaf(1)                                   , 1      ),
-          (Branch(Leaf(1), Leaf(2))                  , 3      ),
-          (Branch(Leaf(1), Branch(Leaf(1), Leaf(2))) , 5      )
-        )
 
+    val dataset =
+      Table(
+        ("tree"                                    ,"size"  ),
+        (Leaf(1)                                   , 1      ),
+        (Branch(Leaf(1), Leaf(2))                  , 3      ),
+        (Branch(Leaf(1), Branch(Leaf(1), Leaf(2))) , 5      )
+      )
+
+    scenario ("size should return the size of the tree"){
       forAll (dataset) { (tree: Tree[Int], size: Int) =>
         Tree.size(tree) shouldEqual size
       }
     }
+
+    scenario ("sizeViaFold should return the size of the tree"){
+      forAll (dataset) { (tree: Tree[Int], size: Int) =>
+        Tree.sizeViaFold(tree) shouldEqual size
+      }
+    }
+
   }
 
 
   feature ("The maximum function"){
     import fpinscala.datastructures.Tree._
-    scenario ("should return the max value of tree"){
 
-      val dataset =
-        Table(
-          ("tree"                                    ,"max"   ),
-          (Leaf(1)                                   , 1      ),
-          (Branch(Leaf(1), Leaf(2))                  , 2      ),
-          (Branch(Leaf(1), Branch(Leaf(1), Leaf(4))) , 4      )
-        )
+    val dataset =
+      Table(
+        ("tree"                                    ,"max"   ),
+        (Leaf(1)                                   , 1      ),
+        (Branch(Leaf(1), Leaf(2))                  , 2      ),
+        (Branch(Leaf(1), Branch(Leaf(1), Leaf(4))) , 4      )
+      )
 
+    scenario ("max should return the max value of tree"){
       forAll (dataset) { (tree: Tree[Int], max: Int) =>
         Tree.maximum(tree) shouldEqual max
+      }
+    }
+
+
+    scenario ("maxViaFold should return the max value of tree"){
+      forAll (dataset) { (tree: Tree[Int], max: Int) =>
+        Tree.maximumViaFold(tree) shouldEqual max
       }
     }
   }
 
   feature ("The depth function"){
     import fpinscala.datastructures.Tree._
-    scenario ("should return the max depth of tree"){
 
-      val dataset =
-        Table(
-          ("tree"                                                                      ,"max"   ),
-          (Leaf(1)                                                                     , 1      ),
-          (Branch(Leaf(1), Leaf(1))                                                    , 2      ),
-          (Branch(Leaf(1), Branch(Leaf(1), Branch(Leaf(1), Branch(Leaf(3), Leaf(2))))) , 5      )
-        )
+    val dataset =
+      Table(
+        ("tree"                                                                      ,"depth" ),
+        (Leaf(1)                                                                     , 1      ),
+        (Branch(Leaf(1), Leaf(1))                                                    , 2      ),
+        (Branch(Leaf(1), Branch(Leaf(1), Branch(Leaf(1), Branch(Leaf(3), Leaf(2))))) , 5      )
+      )
 
+    scenario ("depth should return the max depth of tree"){
       forAll (dataset) { (tree: Tree[Int], max: Int) =>
         Tree.depth(tree) shouldEqual max
+      }
+    }
+
+    scenario ("depthViaFold should return the max depth of tree"){
+      forAll (dataset) { (tree: Tree[Int], max: Int) =>
+        Tree.depthViaFold(tree) shouldEqual max
       }
     }
   }
@@ -67,19 +88,25 @@ class TreeTest extends FeatureSpec with Matchers {
 
   feature ("The map function"){
     import fpinscala.datastructures.Tree._
-    scenario ("should return the mapped f to each leaf"){
 
-      def f = (i: Int) => 2 * i
+    def f = (i: Int) => 2 * i
 
-      val dataset =
-        Table(
-          ("tree",                  "f", "out"                   ),
-          (Leaf(1),                   f, Leaf(2)                  ),
-          (Branch(Leaf(2), Leaf(2)),  f, Branch(Leaf(4), Leaf(4)) )
-        )
+    val dataset =
+      Table(
+        ("tree",                  "f", "out"                   ),
+        (Leaf(1),                   f, Leaf(2)                  ),
+        (Branch(Leaf(2), Leaf(2)),  f, Branch(Leaf(4), Leaf(4)) )
+      )
 
+    scenario ("map should return the mapped f to each leaf"){
       forAll (dataset) { (in: Tree[Int], f: (Int)=> Int,  out: Tree[Int]) =>
         Tree.map(in)(f) shouldEqual out
+      }
+    }
+
+    scenario ("mapViaFold should return the mapped f to each leaf"){
+      forAll (dataset) { (in: Tree[Int], f: (Int)=> Int,  out: Tree[Int]) =>
+        Tree.mapViaFold(in)(f) shouldEqual out
       }
     }
   }
